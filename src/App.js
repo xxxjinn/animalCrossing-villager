07dd7f41c33bdfadffc8600ed69b599a9dae7270
xxxjinn <tlsguswls319@naver.com>
2023-10-14 00:19:16 +0900

    feat: change header position

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import Header from "./components/Header";
 
 export default class App extends Component {
   render() {
-    const routerView = document.createElement("router-view");
     const header = new Header().el;
+    /**실제 페이지 영역 */
+    const routerView = document.createElement("router-view");
 
     this.el.append(header, routerView);
   }
