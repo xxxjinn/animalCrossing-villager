500598712368d3a086f779c8612c5c4cdeb4d8c2
xxxjinn <tlsguswls319@naver.com>
2023-10-15 03:11:33 +0900

    feat: impl notFound page

--- src/routes/NotFound.js
+++ src/routes/NotFound.js
@@ -3,7 +3,11 @@ import { Component } from "../core/component";
 export default class NotFound extends Component {
   render() {
     this.el.innerHTML = /* html */ `
-    <div>not found page</div>
-        `;
+    <div class="wrap">
+      <p class="icon">😿</p>
+      <h1 class="head-text">404 Not Found</h1>
+      <p class="main-text">요청하신 페이지의 경로가 올바르지 않습니다.</p>
+    </div>
+    `;
   }
 }
