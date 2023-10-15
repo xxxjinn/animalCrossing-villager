a8f4685209dcc663f8b25f1c298134eb8fd01d29
xxxjinn <tlsguswls319@naver.com>
2023-10-15 19:49:58 +0900

    feat: connect firebase to home

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,33 +1,73 @@
+import { fetchDataFromFirestore } from "../store/memberStore";
 import { Component } from "../core/component";
 
 export default class Home extends Component {
+  constructor() {
+    super();
+  }
+
+  renderVillagers(villagerData) {
+    const villagersList = this.el.querySelector(".villagers-list");
+
+    villagerData.forEach((data) => {
+      const { name, favoriteColor, imageUrl, sex, birthday, personality } =
+        data;
+
+      const villagerHTML = /* html */ ` 
+        <div class="villager">
+          <ul class="villager-info-ul">
+            <li class="villager-info-li">
+              <img class="villager-img" src="${imageUrl}" />
+            </li>
+            <li class="villager-info-li"><p>${name}</p></li>
+            <li class="villager-info-li"><p class="sex">${sex}</p></li>
+            <li class="villager-info-li"><p>${birthday}</p></li>
+            <li class="villager-info-li"><p>${personality}</p></li>
+            <li class="villager-info-li"><p>${favoriteColor}</p></li>
+          </ul>
+          <div class="delete-villager">
+            <button class="delete-button">주민 삭제</button>
+          </div>
+        </div>
+      `;
+
+      villagersList.insertAdjacentHTML("beforeend", villagerHTML);
+    });
+  }
+
+  async componentDidMount() {
+    const villagerData = await fetchDataFromFirestore();
+    this.renderVillagers(villagerData);
+  }
+
   render() {
     this.el.innerHTML = /* html */ `
-    <main class="main">
-      <div class="main-wrap">
-        <div class="main-top">
-          <h1 class="main-top-text">주민 목록😺</h1>
-          <a href="#/add">
-            <button class="add-button">새로운 주민 등록하기</button></a
-          >
-        </div>
-        <div class="main-middle">
-          <div class="main-middle-header">
-            <hr class="main-middle-hr" />
-            <ul class="villagers-info-ul">
-              <li><p class="villagers-info-li">프로필 사진</p></li>
-              <li><p class="villagers-info-li">이름</p></li>
-              <li><p class="villagers-info-li">성별</p></li>
-              <li><p class="villagers-info-li">생일</p></li>
-              <li><p class="villagers-info-li">성격</p></li>
-              <li><p class="villagers-info-li">좋아하는 색</p></li>
-            </ul>
-            <hr class="main-middle-hr" />
+      <main class="main">
+        <div class="main-wrap">
+          <div class="main-top">
+            <h1 class="main-top-text">주민 목록😺</h1>
+            <a href="#/add">
+              <button class="add-button">새로운 주민 등록하기</button>
+            </a>
           </div>
-          <div class="villagers-list"></div>
+          <div class="main-middle">
+            <div class="main-middle-header">
+              <hr class="main-middle-hr" />
+              <ul class="villagers-info-ul">
+                <li><p class="villagers-info-li">프로필 사진</p></li>
+                <li><p class="villagers-info-li">이름</p></li>
+                <li><p class="villagers-info-li">성별</p></li>
+                <li><p class="villagers-info-li">생일</p></li>
+                <li><p class="villagers-info-li">성격</p></li>
+                <li><p class="villagers-info-li">좋아하는 색</p></li>
+              </ul>
+              <hr class="main-middle-hr" />
+            </div>
+            <div class="villagers-list">
+            </div>
+          </div>
         </div>
-      </div>
-    </main>
-        `;
+      </main>
+    `;
   }
 }
---
+++ src/store/memberStore.js
@@ -0,0 +1,25 @@
+import { db } from "../firebase";
+import { getDocs, collection } from "firebase/firestore";
+
+export const fetchDataFromFirestore = async () => {
+  try {
+    const querySnapshot = await getDocs(collection(db, "villager"));
+    const villagerData = [];
+
+    querySnapshot.forEach((doc) => {
+      const data = doc.data();
+      villagerData.push({
+        name: data.name,
+        favoriteColor: data.favoriteColor,
+        imageUrl: data.imageUrl,
+        sex: data.sex,
+        birthday: data.birthday,
+        personality: data.personality,
+      });
+    });
+
+    return villagerData;
+  } catch (error) {
+    console.error(error);
+  }
+};
